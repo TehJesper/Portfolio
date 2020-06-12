737a472542e3a40c4cfdfda503f2b7113ed977ef
Jesper123 <Jesper8913@gmail.com>
2020-06-12 12:17:43 +0200

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app['request']->server->set('HTTPS', true);
         \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
