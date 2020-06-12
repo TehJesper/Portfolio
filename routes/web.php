0a0d0564788ac3654b3a2da88d3736445ae75ef7
Jesper123 <Jesper8913@gmail.com>
2020-06-12 12:06:36 +0200

    change https

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,8 @@ Route::group([['middleware' => ['auth']], ['scheme' => 'https']], function (){
     Route::get('/articles/{article}/destroy', 'ArticlesController@destroy')->name('articles.destroy');
     Route::get('/randomarticle', 'ArticlesController@random');
 });
-Route::group(['scheme' => 'https'], function (){
-    Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index')->middleware('auth');
+Route::group(['scheme' => 'https', 'middleware' => ['auth']], function (){
+    Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
     //Route::get('/dashboard/{dashboard}/edit', 'DashboardController@edit')->name('dashboard.edit');
     //Route::put('/dashboard/{dashboard}', 'DashboardController@update');
     Route::get('/dashboard/{dashboard}/destroy', 'DashboardController@destroy')->name('dashboard.destroy');
